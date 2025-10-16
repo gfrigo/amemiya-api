8ac2f552948c3f96b78b5e182fa85dede778134b
felipeacarvalho <flpalmeidac@gmail.com>
2025-10-16 14:55:01 -0300

    Creating user register function

--- src/map.py
+++ src/map.py
@@ -13,11 +13,8 @@ fetch: dict = {
     }
 }
 
-def assemble_fetch(type_fetch: str, query_filter: str = "", fetch_map: dict = None):
-    if fetch_map is None:
-        fetch_map = fetch
-
-    type_map: dict = fetch_map[type_fetch]
+def assemble_fetch(type_fetch: str, mapping: dict, query_filter: str = ""):
+    type_map: dict = mapping[type_fetch]
 
     if not query_filter:
         where_stmt: str = ""
@@ -38,15 +35,4 @@ register: dict = {
     }
 }
 
-def assemble_register(type_register: str, values: tuple = None, register_map: dict = None):
-    if register_map is None:
-        register_map = register
-
-    type_map: dict = register_map[type_register]
-
-    register_stmt: str = f"INSERT INTO {type_map["table"]} {type_map["fields"]}"
-
-    print(register_stmt)
-
-assemble_register("user")
 
--- src/register.py
+++ src/register.py
@@ -1,3 +1,35 @@
-from src.database import query_from_table, query_from_string, insert
+from src.database import query_from_table, query_from_string, Insert
+from src.map import register
 
-def user():
+def assemble_register(mapping: dict, values: tuple = None):
+
+        register_stmt: str = f"INSERT INTO {mapping["table"]} ({', '.join(mapping["fields"])}) VALUES ({', '.join(['%s'] * len(mapping["fields"]))})"
+
+        return (len(mapping["fields"]), register_stmt)
+
+class Register:
+    """
+        Class to handle registrations to the database based on predefined patterns.
+    """
+
+    @staticmethod
+    def user(cursor, values:tuple, mapping: dict = register["user"]) -> None:
+        """
+            Registers a new user in the database.
+
+            Args:
+                cursor: Database cursor to execute the insertion.
+                values (*tuple*): A tuple containing the values to be inserted into the user table, should contain: \n'("name", "innerRegister", "password", "email", "telephone", "roleId", "admin", "companyId", "imagePath", "activeUser")'
+
+            Raises:
+                ValueError: If the number of fields and values do not match
+        """
+        fields_length, register_stmt = assemble_register(mapping, values)
+        
+        if len(values) != fields_length:
+            raise ValueError("Fields and Values have different lengths")
+        
+        print(register_stmt)
+        print(values)
+
+        Insert.from_string(cursor, register_stmt, values)
