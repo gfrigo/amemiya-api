f657b6bd468e04d76e79e85545662b6c17e716ba
Felipe Almeida de Carvalho <flpalmeidac@gmail.com>
2025-10-27 18:41:35 -0300

    added fetch and working on add endpoint for attachments

--- src/endpoints/attachment/model.py
+++ src/endpoints/attachment/model.py
@@ -3,13 +3,7 @@ from pydantic import BaseModel
 class AttachmentDataRequest(BaseModel):
     company_id: int | None = None
     user_id: int | None = None
-    user_name: str | None = None
-    inner_register: str | None = None
-    password: str | None = None
-    email: str | None = None
-    telephone: str | None = None
-    role_id: int | None = None
-    admin: bool | None = None
-    company_id: int | None = None
-    image_path: str | None = None
-    active_user: bool | None = None
+    file_data: bytes | None = None
+    file_type: str | None = None
+    attachment_type: str | None = None
+    upload_date: str | None = None
--- src/endpoints/attachment/service.py
+++ src/endpoints/attachment/service.py
@@ -1,32 +1,51 @@
 from src.core.logging_config import logger
 from src.core.database import start_connection, start_cursor
-from src.endpoints.user.repository import UserRepository
-from src.endpoints.user.model import UserDataRequest
+from .repository import AttachmentRepository
+from .model import AttachmentDataRequest
 from src.core.utils import check_missing_fields
 from src.core.config import settings
 
 
-def fetch_attachment_service(company_id: int, user_id: int | None) -> dict:
+def fetch_attachment_service(company_id: int,
+                             user_id: int | None = None,
+                             attachment_type: str | None = None,
+                             date_range_start: str | None = None,
+                             date_range_end: str | None = None) -> dict:
+
     logger.info("FETCH ATTACHMENT SERVICE HIT")
 
+    query_filter = {
+            "uploaded_by_company_id": {"type": "index",
+                                       "value": company_id,
+                                       "table": "Attachments"},
+            "uploaded_by_user_id": {"type": "index",
+                                    "value": user_id,
+                                    "table": "Attachments"},
+            "attachment_type": {"type": "index",
+                                "value": attachment_type,
+                                "table": "Attachments"},
+            "upload_date": {"type": "date_range",
+                            "value": (date_range_start, date_range_end),
+                            "table": "Attachments"}
+        }
+
     with start_connection(settings.DB_HOST, settings.DB_USER, settings.DB_PASSWORD, settings.DB_SCHEMA) as conn:
         with start_cursor(conn) as cursor:
-            result: dict = AttachmentRepository.fetch(cursor, data)
+
+            result: dict = AttachmentRepository.fetch(cursor, query_filter)
+
             return result
 
-def add_user_service(request: UserDataRequest, conn = None, cursor = None):
-    logger.info("ADD USER SERVICE HIT")
-    data = request.model_dump()
+def add_attachment_service(request: AttachmentDataRequest):
+    logger.info("ADD ATTACHMENT SERVICE HIT")
     required_fields = ["user_name", "password", "email", "telephone", "role_id", "company_id"]
-    check_missing_fields(data, required_fields)
 
     with start_connection(settings.DB_HOST, settings.DB_USER, settings.DB_PASSWORD, settings.DB_SCHEMA) as conn:
         with start_cursor(conn) as cursor:
-            UserRepository.add(cursor, data)
             conn.commit()
     return "User added successfully"
 
-def edit_user_service(request: UserDataRequest, conn = None, cursor = None):
+def edit_user_service(request: AttachmentDataRequest, conn = None, cursor = None):
     logger.info("EDIT USER SERVICE HIT")
     data = request.model_dump()
     required_fields = ["user_id"]
@@ -34,15 +53,15 @@ def edit_user_service(request: UserDataRequest, conn = None, cursor = None):
 
     with start_connection(settings.DB_HOST, settings.DB_USER, settings.DB_PASSWORD, settings.DB_SCHEMA) as conn:
         with start_cursor(conn) as cursor:
-            user_data: dict = UserRepository.fetch(cursor, data)
+            user_data: dict = AttachmentRepository.fetch(cursor, data)
             if not user_data:
                 return "user_id has no data"
 
-            UserRepository.edit(cursor, data)
+            AttachmentRepository.edit(cursor, data)
             conn.commit()
     return "User edited successfully"
 
-def remove_user_service(request: UserDataRequest, conn = None, cursor = None):
+def remove_user_service(request: AttachmentDataRequest, conn = None, cursor = None):
     logger.info("REMOVE USER SERVICE HIT")
     data = request.model_dump()
     required_fields = ["user_id"]
@@ -50,10 +69,10 @@ def remove_user_service(request: UserDataRequest, conn = None, cursor = None):
 
     with start_connection(settings.DB_HOST, settings.DB_USER, settings.DB_PASSWORD, settings.DB_SCHEMA) as conn:
         with start_cursor(conn) as cursor:
-            user_data: dict = UserRepository.fetch(cursor, data)
+            user_data: dict = AttachmentRepository.fetch(cursor, data)
             if not user_data:
                 return "user_id has no data"
 
-            UserRepository.remove(cursor, data)
+            AttachmentRepository.remove(cursor, data)
             conn.commit()
     return "User removed successfully"
