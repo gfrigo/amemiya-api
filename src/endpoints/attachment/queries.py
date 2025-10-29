c869237c412aef5a4b0614f78a90f26b753946da
Felipe Almeida de Carvalho <flpalmeidac@gmail.com>
2025-10-28 22:18:35 -0300

    finished add attachment logic

--- src/endpoints/attachment/queries.py
+++ src/endpoints/attachment/queries.py
@@ -5,21 +5,31 @@ ATTACHMENTS = Table("Attachments")
 COMPANIES = Table("Companies")
 USERS = Table("Users")
 
+class AssembleStatement:
 
-def get_data(query_filter) -> str:
-    query = (
-        MySQLQuery.from_(ATTACHMENTS).select(
-            ATTACHMENTS.attachment_id,
-            COMPANIES.company_name,
-            USERS.user_name,
-            ATTACHMENTS.file_data,
-            ATTACHMENTS.file_type,
-            ATTACHMENTS.attachment_type,
-            ATTACHMENTS.upload_date
+    @staticmethod
+    def get_attachment_data(query_filter) -> str:
+        stmt = (
+            MySQLQuery.from_(ATTACHMENTS).select(
+                ATTACHMENTS.attachment_id,
+                COMPANIES.company_name,
+                USERS.user_name,
+                ATTACHMENTS.file_data,
+                ATTACHMENTS.file_type,
+                ATTACHMENTS.attachment_type,
+                ATTACHMENTS.upload_date
+            )
+            .left_join(COMPANIES).on(ATTACHMENTS.uploaded_by_company_id == COMPANIES.company_id)
+            .left_join(USERS).on(ATTACHMENTS.uploaded_by_user_id == USERS.user_id)
+            .where(assemble_condition(query_filter))
         )
-        .left_join(COMPANIES).on(ATTACHMENTS.uploaded_by_company_id == COMPANIES.company_id)
-        .left_join(USERS).on(ATTACHMENTS.uploaded_by_user_id == USERS.user_id)
-        .where(assemble_condition(query_filter))
-    )
 
-    return query.get_sql()
+        return stmt.get_sql()
+
+    @staticmethod
+    def add_attachment(data: dict) -> str:
+        columns = ", ".join(data.keys())
+        placeholders = ", ".join(["%s"] * len(data))
+        stmt = f"INSERT INTO `Attachments` (`uploaded_by_company_id`, `uploaded_by_user_id`, `file_data`, `file_type`, `attachment_type`, `upload_date`) VALUES ({placeholders})"
+
+        return stmt
--- src/endpoints/attachment/repository.py
+++ src/endpoints/attachment/repository.py
@@ -1,7 +1,7 @@
-from .queries import get_data
+from .queries import AssembleStatement
 from src.core.logging_config import logger
 from src.register import Register
-
+from base64 import b64encode
 
 
 class AttachmentRepository:
@@ -11,16 +11,39 @@ class AttachmentRepository:
         logger.info("FETCH ATTACHMENT REPOSITORY HIT")
 
         try:
-            select_stmt = get_data(query_filter)
+            select_stmt = AssembleStatement.get_attachment_data(query_filter)
+
+            logger.info(f"To execute: {select_stmt}")
 
             cursor.execute(select_stmt)
+
+            logger.info("Executed")
+
             result = cursor.fetchall()
 
             if not result:
                 return None
 
-            # TODO
-            return {}
+            data: dict = {}
+            for entry in result:
+                attachment_id, company_name, user_name, file_data, file_type, attachment_type, upload_date = entry
+
+                encoded_file_data = b64encode(file_data).decode("utf-8")
+
+                file_name = f"test_output.{file_type}"
+                with open(file_name, "wb") as f:
+                    f.write(file_data)
+
+                data[attachment_id] = {
+                    "company_name": company_name,
+                    "user_name": user_name,
+                    "file_data": encoded_file_data,
+                    "file_type": file_type,
+                    "attachment_type": attachment_type,
+                    "upload_date": upload_date
+                }
+
+            return data
 
         except IndexError:
             return None
@@ -28,20 +51,21 @@ class AttachmentRepository:
 
     @staticmethod
     def add(cursor, data: dict):
-        logger.info("ADD USER REPOSITORY HIT")
+        logger.info("ADD ATTACHMENT REPOSITORY HIT")
 
-        Register.add(cursor, "user", (
-            data["user_name"],
-            data["inner_register"],
-            data["password"],
-            data["email"],
-            data["telephone"],
-            str(data["role_id"]),
-            data["admin"] if data["admin"] else '0',
-            str(data["company_id"]),
-            data["image_path"] or 'assets/profiles/default.png',
-            '1'
-        ))
+        try:
+
+            insert_stmt = AssembleStatement.add_attachment(data)
+
+            logger.info(f"To execute: {insert_stmt}")
+
+            cursor.execute(insert_stmt, tuple(data.values()))
+
+            logger.info("Executed")
+
+        except Exception as e:
+            print(e)
+            return None
 
     @staticmethod
     def edit(cursor, data: dict):
