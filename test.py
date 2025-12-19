e887d8a136ba268de173df093c00b2b5226b06fd
Felipe Almeida de Carvalho <flpalmeidac@gmail.com>
2025-12-19 19:47:11 -0300

    commit teste

--- test.py
+++ test.py
@@ -4,21 +4,10 @@ from dotenv import load_dotenv
 from pathlib import Path
 from src.core.database import start_connection, start_cursor, Statements
 from src.register import Register
+import reverse_geocode as rg
 
-env_path: Path = Path(".env")
-load_dotenv(env_path)
+coords = (-23.5505, -46.6333)
 
-DB_HOST: str = os.getenv("DB_HOST")
-DB_USER: str = os.getenv("DB_USER")
-DB_PASSWORD: str = os.getenv("DB_PASSWORD")
-DB_SCHEMA: str | None = os.getenv("DB_SCHEMA")
+result = rg.search([coords])
 
-with start_connection(DB_HOST, DB_USER, DB_PASSWORD, DB_SCHEMA) as conn:
-    with start_cursor(conn) as cursor:
-
-        with open("test.pdf", "rb") as file:
-            binary_data = file.read()
-
-        cursor.execute("INSERT INTO Attachment (attachment_data, attachment_type) VALUES (%s, %s);", (binary_data, "pdf"))
-
-        conn.commit()
+print(result)
